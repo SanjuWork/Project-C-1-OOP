6cf0ef069ebddfd2cccf442e2392de487bd0d9b7
Sanju Kumari <63475754+SanjuWork@users.noreply.github.com>
2020-04-16 17:47:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+//object oriented concepts 
+function setup() {
+  createCanvas(600,400);
+ 
+  bubble1 = new Bubble();
+  bubble2 = new Bubble();
+  bubble3 = new Bubble();
+
+}
+
+function draw() {
+  background(0);
+  bubble1.move();
+  bubble2.move();
+  bubble3.move();
+ 
+  bubble1.display();
+  bubble2.display();
+  bubble3.display();
+}
